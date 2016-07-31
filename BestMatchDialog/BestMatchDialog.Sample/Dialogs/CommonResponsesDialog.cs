54dbc00f1bd54acf6d4535767f2b94dd6ec485be
Gary Pretty <gary@garypretty.co.uk>
2016-07-31 23:31:18 +0100

    Initial commit of the BestMatchDialog project including Sample

---
+++ BestMatchDialog/BestMatchDialog.Sample/Dialogs/CommonResponsesDialog.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace BestMatchDialog.Sample.Dialogs
+{
+    [Serializable]
+    public class CommonResponsesDialog : BestMatchDialog<object>
+    {
+        [BestMatch(new string[] { "Hi", "Hi There", "Hello there", "Hey", "Hello",
+            "Hey there", "Greetings", "Good morning", "Good afternoon", "Good evening", "Good day" },
+            threshold: 0.5, ignoreCase: false, ignoreNonAlphaNumericCharacters: false)]
+        public async Task HandleGreeting(IDialogContext context)
+        {
+            await context.PostAsync("Well hello there. What can I do for you today?");
+            context.Wait(MessageReceived);
+        }
+
+        [BestMatch(new string[] { "how goes it", "how do", "hows it going", "how are you",
+            "how do you feel", "whats up", "sup", "hows things" })]
+        public async Task HandleStatusRequest(IDialogContext context)
+        {
+            await context.PostAsync("I am great.");
+            context.Wait(MessageReceived);
+        }
+
+        [BestMatch(new string[] { "bye", "bye bye", "got to go",
+            "see you later", "laters", "adios" })]
+        public async Task HandleGoodbye(IDialogContext context)
+        {
+            await context.PostAsync("Bye. Looking forward to our next awesome conversation already.");
+            context.Wait(MessageReceived);
+        }
+
+        [BestMatch(new string[] { "thank you", "thanks", "much appreciated", "thanks very much", "thanking you" })]
+        public async Task HandleThanks(IDialogContext context)
+        {
+            await context.PostAsync("You're welcome.");
+            context.Wait(MessageReceived);
+        }
+
+        public override async Task NoMatchHandler(IDialogContext context)
+        {
+            await context.PostAsync("I’m not sure what you want.");
+            context.Wait(MessageReceived);
+        }
+    }
+}
---
+++ BestMatchDialog/BestMatchDialog/BestMatchDialog.cs
@@ -0,0 +1,252 @@
+﻿// 
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+// 
+// Gary Pretty Github:
+// https://github.com/GaryPretty
+// 
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+// 
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Internals.Fibers;
+
+namespace BestMatchDialog
+{
+    [Serializable]
+    public class BestMatchDialog<T> : IDialog<T>
+    {
+        [NonSerialized]
+        protected Dictionary<BestMatchAttribute, BestMatchHandler> HandlerByBestMatchLists;
+
+        protected string InitialMessage;
+
+        public BestMatchDialog()
+        {
+            var type = this.GetType();
+        }
+
+        public virtual async Task StartAsync(IDialogContext context)
+        {
+            if (!string.IsNullOrEmpty(InitialMessage))
+            {
+                await HandleMessage(context, InitialMessage);
+            }
+            else
+            {
+                context.Wait(MessageReceived);
+            }
+        }
+
+        protected virtual async Task MessageReceived(IDialogContext context, IAwaitable<IMessageActivity> item)
+        {
+            var message = await item;
+            var messageText = await GetBestMatchQueryTextAsync(context, message);
+            await HandleMessage(context, messageText);
+        }
+
+        private async Task HandleMessage(IDialogContext context, string messageText)
+        {
+            if (this.HandlerByBestMatchLists == null)
+            {
+                this.HandlerByBestMatchLists =
+                    new Dictionary<BestMatchAttribute, BestMatchHandler>(GetHandlersByBestMatchLists());
+            }
+
+            BestMatchHandler handler = null;
+
+            double bestMatchedScore = 0;
+
+            foreach (var handlerByBestMatchList in HandlerByBestMatchLists)
+            {
+                var match = FindBestMatch(handlerByBestMatchList.Key.BestMatchList,
+                    messageText,
+                    handlerByBestMatchList.Key.Threshold,
+                    handlerByBestMatchList.Key.IgnoreCase,
+                    handlerByBestMatchList.Key.IgnoreNonAlphanumericCharacters);
+
+                if (match?.Score > bestMatchedScore)
+                {
+                    bestMatchedScore = match.Score;
+                    handler = handlerByBestMatchList.Value;
+                }
+            }
+
+            if (handler != null)
+            {
+                await handler(context);
+            }
+            else
+            {
+                await NoMatchHandler(context);
+            }
+        }
+
+        protected virtual Task<string> GetBestMatchQueryTextAsync(IDialogContext context, IMessageActivity message)
+        {
+            return Task.FromResult(message.Text);
+        }
+
+        protected virtual IDictionary<BestMatchAttribute, BestMatchHandler> GetHandlersByBestMatchLists()
+        {
+            return EnumerateHandlers(this).ToDictionary(kv => kv.Key, kv => kv.Value);
+        }
+
+        private StringMatch FindBestMatch(IEnumerable<string> choices, string utterance, double threshold = 0.5, bool ignoreCase = true, bool ignoreAlphanumeric = true)
+        {
+            StringMatch bestMatch = null;
+            var matches = FindAllMatches(choices, utterance, threshold, ignoreCase, ignoreAlphanumeric);
+            foreach (var match in matches)
+            {
+                if (bestMatch == null || match.Score > bestMatch.Score)
+                {
+                    bestMatch = match;
+                }
+            }
+            return bestMatch;
+        }
+
+        private List<StringMatch> FindAllMatches(IEnumerable<string> choices, string utterance, double threshold = 0.6, bool ignoreCase = true, bool ignoreNonAlphanumeric = true)
+        {
+            var matches = new List<StringMatch>();
+
+            if (choices == null || !choices.Any())
+                return matches;
+
+            var trimmedUtterance = utterance.Trim();
+            if (ignoreNonAlphanumeric)
+                trimmedUtterance = trimmedUtterance.Replace("?", string.Empty).Replace("'", string.Empty);
+
+            var tokens = utterance.Split(' ');
+
+            foreach (var choice in choices)
+            {
+                double score = 0;
+                var choiceValue = choice.Trim();
+                if (ignoreNonAlphanumeric)
+                    choiceValue = choiceValue.Replace("?", string.Empty).Replace("'", string.Empty);
+
+                if (choiceValue.IndexOf(trimmedUtterance, ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal) >= 0)
+                {
+                    score = trimmedUtterance.Length / choiceValue.Length;
+                }
+                else if (trimmedUtterance.IndexOf(choiceValue) >= 0)
+                {
+                    score = Math.Min(0.5 + (choiceValue.Length / trimmedUtterance.Length), 0.9);
+                }
+                else
+                {
+                    foreach (var token in tokens)
+                    {
+                        var matched = string.Empty;
+
+                        if (choiceValue.IndexOf(token, ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal) >= 0)
+                        {
+                            matched += token;
+                        }
+
+                        score = matched.Length / choiceValue.Length;
+                    }
+                }
+
+                if (score >= threshold)
+                {
+                    matches.Add(new StringMatch { Choice = choiceValue, Score = score });
+                }
+            }
+
+            return matches;
+        }
+
+        internal static IEnumerable<KeyValuePair<BestMatchAttribute, BestMatchHandler>> EnumerateHandlers(object dialog)
+        {
+            var type = dialog.GetType();
+            var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            foreach (var method in methods)
+            {
+                var bestMatchListAttributes = method.GetCustomAttributes<BestMatchAttribute>(inherit: true).ToArray();
+                Delegate created = null;
+                try
+                {
+                    created = Delegate.CreateDelegate(typeof(BestMatchHandler), dialog, method, throwOnBindFailure: false);
+                }
+                catch (ArgumentException)
+                {
+                    // "Cannot bind to the target method because its signature or security transparency is not compatible with that of the delegate type."
+                    // https://github.com/Microsoft/BotBuilder/issues/634
+                    // https://github.com/Microsoft/BotBuilder/issues/435
+                }
+
+                var bestMatchHandler = (BestMatchHandler)created;
+                if (bestMatchHandler != null)
+                {
+                    //var bestMatchLists = bestMatchListAttributes.Select(i => i.BestMatchList).DefaultIfEmpty(null);
+
+                    foreach (var bestMatchListAttribute in bestMatchListAttributes)
+                    {
+                        if (bestMatchListAttribute != null && bestMatchListAttributes.Any())
+                            yield return new KeyValuePair<BestMatchAttribute, BestMatchHandler>(bestMatchListAttribute, bestMatchHandler);
+                    }
+                }
+            }
+        }
+
+        public virtual async Task NoMatchHandler(IDialogContext context)
+        {
+            throw new Exception("No best match found and NoMatchHandler method not overridden");
+        }
+
+        internal class StringMatch
+        {
+            public string Choice { get; set; }
+            public double Score { get; set; }
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class BestMatchAttribute : Attribute
+    {
+        public readonly string[] BestMatchList;
+        public readonly bool IgnoreCase;
+        public readonly bool IgnoreNonAlphanumericCharacters;
+        public readonly double Threshold;
+
+        public BestMatchAttribute(string[] bestMatchList, double threshold = 0.5, bool ignoreCase = true, bool ignoreNonAlphaNumericCharacters = true)
+        {
+            BestMatchList = bestMatchList;
+            IgnoreCase = ignoreCase;
+            IgnoreNonAlphanumericCharacters = ignoreNonAlphaNumericCharacters;
+            Threshold = threshold;
+        }
+    }
+
+    public delegate Task BestMatchHandler(IDialogContext context);
+}
